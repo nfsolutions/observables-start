6d60cf15f41c2f52012072ce96eba232cd324b4e
nydi <stefan.nydegger@gmail.com>
2018-08-10 10:08:15 +0200

    lecture 163

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {Observable, interval} from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {Observable, Observer} from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +8,42 @@ import {Observable, interval} from 'rxjs';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
-    const myNumbers = interval(1000);
-    myNumbers.subscribe(
-      (number: number) => {
-        console.log(number);
+    // const myNumbers = interval(1000);
+    // myNumbers.subscribe(
+    //   (number: number) => {
+    //     console.log(number);
+    //   }
+    // );
+
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      }, 2000),
+        setTimeout(() => {
+          observer.next('second package');
+        }, 4000),
+        setTimeout(() => {
+          // observer.error('This does not work');
+          observer.complete();
+        }, 5000),
+        setTimeout(() => {
+          observer.next('third package');
+        }, 6000);
+    });
+    myObservable.subscribe(
+      (data: string) => {
+        console.log(data);
+      },
+      (error: string) => {
+        console.log(error);
+      },
+      () => {
+        console.log('completed');
       }
     );
   }
-
 }
